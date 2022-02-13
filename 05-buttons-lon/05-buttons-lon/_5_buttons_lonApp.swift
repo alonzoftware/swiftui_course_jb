a7bea4781c58e3e77134b9b1a6d57663f4b369ca
MacMiniM1 <alonzo.choque@icloud.com>
2022-02-13 11:00:56 -0400

    MacMiniM1 20220213 11:00 05-buttons-lon

---
+++ 05-buttons-lon/05-buttons-lon/_5_buttons_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_buttons_lonApp.swift
+//  05-buttons-lon
+//
+//  Created by Rene Alonzo Choque Saire on 13/2/22.
+//
+
+import SwiftUI
+
+@main
+struct _5_buttons_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
