573a08e20595ffae7f5484b6454c35eecb739bf2
MacBookAir <alonzoftware@gmail.com>
2022-04-24 13:08:41 -0400

    MacBookAir 20220424 13:07 Test Successfully 09-transitions-lon

--- 09-transitions-lon/transitions-lon/ContentView.swift
+++ 09-transitions-lon/transitions-lon/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showTranslation = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Rectangle()
+                .frame(width: 250, height: 250)
+                .foregroundColor(.orange)
+                .overlay(
+                    Text("Car")
+                        .font(.system(.largeTitle, design:.rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                )
+            
+            if showTranslation{
+                Rectangle()
+                .frame(width: 250, height: 250)
+                .foregroundColor(.purple)
+                .overlay(
+                    Text("Coche")
+                        .font(.system(.largeTitle, design:.rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                )
+                .transition(.asymmetric(insertion: .scale(scale: 0, anchor: .bottom),
+                                        removal: .offset(x: -600, y: 0)
+                    )
+                )
+            }
+
+        }
+        .onTapGesture {
+            withAnimation(Animation.spring()){
+                self.showTranslation.toggle()
+            }
+        }
+
     }
 }
 
