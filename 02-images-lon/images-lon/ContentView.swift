b6a5680f9fdfafe2a1d723d2b8f3e8dfff7f5880
MacMiniM1 <alonzo.choque@icloud.com>
2022-10-21 12:36:01 -0400

    MacMiniM1 20221021 12:35 02-images-lon Working With Images Part 2

--- 02-images-lon/images-lon/ContentView.swift
+++ 02-images-lon/images-lon/ContentView.swift
@@ -33,6 +33,36 @@ struct ContentView: View {
             //.aspectRatio(contentMode: .fit).frame(width: 300) //Scale to Width
             //.aspectRatio(contentMode: .fill).frame(width: 300).clipped() //Fill and scale to width cutting edges
             .aspectRatio(contentMode: .fill).frame(width: 300).clipShape(Capsule()) //Fill and scale cliped with Shape,Circle(), Ellipse(), Capsule()
+            .opacity(0.5)
+            .overlay(
+//                Image(systemName: "heart.fill")
+//                    .font(.system(size: 60))
+//                    .foregroundColor(.pink)
+                
+//                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
+//                    .fontWeight(.bold)
+//                    .font(.system(.headline,design: .rounded))
+//                    .foregroundColor(.white)
+//                    .padding()
+//                    .background(Color.gray)
+//                    .cornerRadius(10)
+//                    .opacity(0.7)
+//                    .padding()
+//                ,alignment: .bottom
+                
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .opacity(0.5)
+                    .overlay(
+                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.yellow)
+                            .frame(width: 250)
+                    )
+                    
+                    
+            )
     }
 }
 
