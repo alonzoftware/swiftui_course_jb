6441c054fd2bd5b3a6282c1a5e51dcc7c4a98ccd
MacBookAir <alonzo.choque@icloud.com>
2023-03-19 22:32:53 -0400

    MacBookAir2017 20230319 22:32 10-lists-lon :: Lists With Images using Structure

--- 10-lists-lon/lists-lon/ContentView.swift
+++ 10-lists-lon/lists-lon/ContentView.swift
@@ -23,20 +23,60 @@ struct ContentView: View {
         "toad",
         "yoshi"
     ]
+    
+    var games = [
+        Game(name: "The Goombas Game", image: "goomba"),
+        Game(name: "The Mario Game", image: "luigi"),
+        Game(name: "The Luigi Game", image: "mario"),
+        Game(name: "The Toad Adventure", image: "toad"),
+        Game(name: "The Yoshis Story Most Amazing Adventure of his life", image: "yoshi"),
+        
+    ]
     var body: some View {
-        List(gameNames.indices,id: \.self){
-            index in
+        //        OPTION 1
+        //        List(gameNames.indices,id: \.self){
+        //            index in
+        //            HStack{
+        //                Image(self.gameImages[index])
+        //                    .resizable()
+        //                    .aspectRatio(contentMode: .fill)
+        //                    .frame(width: 100, height: 100, alignment: .center)
+        //                    .clipped()
+        //                    .cornerRadius(50)
+        //                    .padding([.top])
+        //                Text("\(self.gameNames[index])")
+        //            }
+        //
+        //        }
+        //        OPTION 2
+        //        List(games.indices,id: \.self){
+        //            index in
+        //            HStack{
+        //                Image(self.games[index].image)
+        //                    .resizable()
+        //                    .aspectRatio(contentMode: .fill)
+        //                    .frame(width: 100, height: 100, alignment: .center)
+        //                    .clipped()
+        //                    .cornerRadius(50)
+        //                    .padding([.top])
+        //                Text("\(self.games[index].name)")
+        //            }
+        //
+        //        }
+        //        OPTION 3
+        List(games,id: \.name){
+            game in
             HStack{
-                Image(self.gameImages[index])
+                Image(game.image)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .frame(width: 100, height: 100, alignment: .center)
                     .clipped()
                     .cornerRadius(50)
                     .padding([.top])
-                Text("\(self.gameNames[index])")
+                Text("\(game.name)")
             }
-
+            
         }
     }
 }
@@ -45,4 +85,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct Game {
+    var name : String
+    var image: String
+    
 }
