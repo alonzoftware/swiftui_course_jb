9ad2366fa6419967999745fe83adeb43a7a9dc42
MacMiniM1 <alonzo.choque@icloud.com>
2023-04-25 12:49:15 -0400

    MacMiniM1 20230425 12:48 14-sign_up :: Create Project

---
+++ 14-sign_up-lon/sign_up-lon/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  sign_up-lon
+//
+//  Created by Rene Alonzo Choque Saire on 25/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject private var registrationVM = RegistrationViewModel()
+    var body: some View {
+        VStack{
+            Text("Crea una cuenta")
+                .font(.system(.largeTitle, design: .rounded))
+                .bold()
+                .padding(.bottom, 50)
+            SingleFormView(fieldName: "Nombre de Usuario", fieldValue: $registrationVM.username)
+                .padding()
+        }
+    }
+    struct SingleFormView: View {
+        
+        var fieldName = ""
+        @Binding var fieldValue: String
+        var isProtected = false
+        
+        var body: some View{
+            VStack{
+                if isProtected{
+                    SecureField(fieldName, text: $fieldValue)
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                        .padding(.horizontal)
+                }else{
+                    TextField(fieldName, text: $fieldValue)
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                        .padding(.horizontal)
+                }
+                Divider()
+                    .frame(height: 1)
+                    .background(Color.gray)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 14-sign_up-lon/sign_up-lon/sign_up_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  sign_up_lonApp.swift
+//  sign_up-lon
+//
+//  Created by Rene Alonzo Choque Saire on 25/4/23.
+//
+
+import SwiftUI
+
+@main
+struct sign_up_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
