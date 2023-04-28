e323c0f249d4fa66227fa5e6802a3a41ab4260ba
MacBookAir <alonzo.choque@icloud.com>
2023-04-28 18:56:03 -0400

    MacBookAir2017 20230428 18:55 14-sign_up :: Add Validate Views

--- 14-sign_up-lon/sign_up-lon/ContentView.swift
+++ 14-sign_up-lon/sign_up-lon/ContentView.swift
@@ -16,8 +16,60 @@ struct ContentView: View {
                 .bold()
                 .padding(.bottom, 50)
             SingleFormView(fieldName: "Nombre de Usuario", fieldValue: $registrationVM.username)
+            
+            ValidationFormView(
+                iconName: registrationVM.usernameLengthValid ? "checkmark.circle" : "xmark.circle",
+                iconColor: registrationVM.usernameLengthValid ? Color.green : Color.red,
+                formText: "Mínimo 6 caracteres",
+                conditionChecked: registrationVM.usernameLengthValid)
+            .padding()
+            
+            
             SingleFormView(fieldName: "Clave de Acceso", fieldValue: $registrationVM.password, isProtected: true)
+            VStack{
+                ValidationFormView(
+                    iconName: registrationVM.passwordLengthValid ? "checkmark.circle" : "xmark.circle",
+                    iconColor: registrationVM.passwordLengthValid ? Color.green : Color.red,
+                    formText: "Mínimo 8 caracteres",
+                    conditionChecked: registrationVM.passwordLengthValid)
+                ValidationFormView(
+                    iconName: registrationVM.passwordCapitalLetter ? "checkmark.circle" : "xmark.circle",
+                    iconColor: registrationVM.passwordCapitalLetter ? Color.green : Color.red,
+                    formText: "Una mayúscula y una minúscula",
+                    conditionChecked: registrationVM.passwordCapitalLetter)
+            }.padding()
             SingleFormView(fieldName: "Confirmar Clave de Acceso", fieldValue: $registrationVM.confirmPassword, isProtected: true)
+            ValidationFormView(
+                iconName: registrationVM.passwordsMatch ? "checkmark.circle" : "xmark.circle",
+                iconColor: registrationVM.passwordsMatch ? Color.green : Color.red,
+                formText: "Las dos contraseñas deben coincidir",
+                conditionChecked: registrationVM.passwordsMatch)
+            .padding()
+            .padding(.bottom, 30)
+            Button(action: {
+                //TODO: proceder a la siguiente pantalla
+            }){
+                Text("Registrarme")
+                    .font(.system(.title, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.yellow]), startPoint: .top, endPoint: .bottom))
+                    .cornerRadius(8)
+            }
+            
+            HStack{
+                Text("¿Ya tienes cuenta?")
+                    .font(.system(.body, design: .rounded))
+                    .bold()
+                Button(action: {
+                    //TODO: proceder a la pantalla de log in
+                }){
+                    Text("Entrar ahora")
+                        .font(.system(.headline))
+                        .foregroundColor(Color.green)
+                }
+            }.padding()
             Spacer()
         }.padding()
     }
@@ -46,8 +98,28 @@ struct ContentView: View {
         }
     }
 }
+struct ValidationFormView: View {
+    
+    var iconName = "xmark.circle"
+    var iconColor = Color(red: 0.9, green: 0.5, blue: 0.5)
+    var formText = ""
+    var conditionChecked = false
+    
+    var body: some View{
+        HStack{
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+            Text(formText)
+                .font(.system(.body, design: .rounded))
+                .foregroundColor(.gray)
+                .strikethrough(conditionChecked)
+            Spacer()
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
