2de6c2d73ca745c29e38e01f29254c37c634b3cf
MacBookAir <alonzo.choque@icloud.com>
2023-05-02 19:31:26 -0400

    MacBookAir2017 20230502 19:25 15-gestos-lon :: Create Project and First Gesture

---
+++ 15-gestos-lon/gestos-lon/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  gestos-lon
+//
+//  Created by Rene Alonzo Choque Saire on 2/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var hasBeenPressed = false
+    var body: some View {
+        Image(systemName: "paperplane.fill")
+                    .font(.system(size:150))
+                    .foregroundColor(.purple)
+//                    .opacity(hasBeenLongPressed ? 0.5 : 1.0)
+                    .scaleEffect(hasBeenPressed ? 2.0 : 1.0)
+                    .animation(.easeIn,value: self.hasBeenPressed)
+                    .gesture(
+//                        TapGesture()
+//                            .onEnded{
+//                                print("GESTURE : TapGesture")
+//                                self.hasBeenPressed.toggle()
+//                            }
+                        
+                        LongPressGesture(minimumDuration: 2.0)
+//                            .updating($hasBeenLongPressed){ (value, state, transaction) in
+//                                state = value
+//                            }
+                            .onEnded{ _ in
+                                self.hasBeenPressed.toggle()
+                            }
+                    )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 15-gestos-lon/gestos-lon/gestos_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  gestos_lonApp.swift
+//  gestos-lon
+//
+//  Created by Rene Alonzo Choque Saire on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct gestos_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
