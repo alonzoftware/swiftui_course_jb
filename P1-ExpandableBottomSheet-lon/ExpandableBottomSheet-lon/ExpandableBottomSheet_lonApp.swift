93b2b03c77d3e4204662cd4ccdeb1513a05a328b
MacBookAir <alonzo.choque@icloud.com>
2023-05-11 18:09:57 -0400

    MacBookAir2017 20230511 18:09 P1-ExpandableBottomSheet :: Create Project

---
+++ P1-ExpandableBottomSheet-lon/ExpandableBottomSheet-lon/ExpandableBottomSheet_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableBottomSheet_lonApp.swift
+//  ExpandableBottomSheet-lon
+//
+//  Created by Rene Alonzo Choque Saire on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableBottomSheet_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
