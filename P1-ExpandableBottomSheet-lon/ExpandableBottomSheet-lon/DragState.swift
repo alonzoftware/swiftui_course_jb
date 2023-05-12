8a089f38023f09312b6def4ce0dab569af20b015
MacMiniM1 <alonzo.choque@icloud.com>
2023-05-12 11:39:16 -0400

    MacMiniM1 20230512 11:38 P1-ExpandableBottomSheet-lon :: Create CourseDetailView

---
+++ P1-ExpandableBottomSheet-lon/ExpandableBottomSheet-lon/DragState.swift
@@ -0,0 +1,44 @@
+//
+//  DragState.swift
+//  ExpandableBottomSheet-lon
+//
+//  Created by Rene Alonzo Choque Saire on 12/5/23.
+//
+
+
+import Foundation
+import CoreGraphics
+
+enum DragState {
+    case none
+    case pressing
+    case dragging(translation: CGSize)
+    
+    var translation: CGSize{
+        switch self {
+        case .none, .pressing:
+            return .zero
+        case .dragging(let translation):
+            return translation
+        }
+    }
+    
+    var isPressing: Bool{
+        switch self {
+        case .pressing, .dragging:
+            return true
+        case .none:
+            return false
+        }
+    }
+    
+    var isDragging: Bool {
+        switch self {
+        case .dragging:
+            return true
+        default:
+            return false
+        }
+    }
+    
+}
